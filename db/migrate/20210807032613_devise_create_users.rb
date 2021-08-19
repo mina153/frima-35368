7318f114f0f7b36c1b5311d867fc440ac96fe382
mina153 <popy0125@icloud.com>
2021-08-19 12:33:20 +0900

    修正版

--- db/migrate/20210807032613_devise_create_users.rb
+++ db/migrate/20210807032613_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name, null: false
       t.string :family_name_reading, null: false
       t.string :first_name_reading, null: false
-      t.date :birth_day, null:false
+      t.date :birth_date, null:false
 
       ## Recoverable
       t.string   :reset_password_token
